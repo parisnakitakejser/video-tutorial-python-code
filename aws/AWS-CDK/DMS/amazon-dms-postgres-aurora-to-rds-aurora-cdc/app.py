bf404a8fa8a41035421d962fdc24075d4a49d931
Paris Nakita Kejser <paris.kejser@eb.dk>
2023-03-22 12:09:40 +0100

    updated version

--- aws/AWS-CDK/DMS/amazon-dms-postgres-aurora-to-rds-aurora-cdc/app.py
+++ aws/AWS-CDK/DMS/amazon-dms-postgres-aurora-to-rds-aurora-cdc/app.py
@@ -1,7 +1,6 @@
 from aws_cdk import (
     Stack,
     App,
-    Environment,
     RemovalPolicy,
     CfnOutput,
     aws_ec2 as ec2,
@@ -16,6 +15,8 @@ from json import dumps
 class PostgresAuroraToInstanceStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+
+        database_name = "testing_db"
 
         # Create a VPC for the RDS instances
         vpc = ec2.Vpc(
@@ -36,23 +37,30 @@ class PostgresAuroraToInstanceStack(Stack):
             ],
         )
 
-
         # Define the security group for the Aurora cluster
-        source_cluster_sg = ec2.SecurityGroup(self, "SourceClusterSG", vpc=vpc, description="Security group for Aurora cluster")
-        source_cluster_sg.add_ingress_rule(ec2.Peer.ipv4(vpc.vpc_cidr_block), ec2.Port.tcp(5432), "Allow access from the VPC")
+        source_cluster_sg = ec2.SecurityGroup(
+            self,
+            "SourceClusterSG",
+            vpc=vpc,
+            description="Security group for Aurora cluster",
+        )
+        source_cluster_sg.add_ingress_rule(
+            ec2.Peer.ipv4(vpc.vpc_cidr_block),
+            ec2.Port.tcp(5432),
+            "Allow access from the VPC",
+        )
 
         # Define the security group for the RDS instance
-        target_instance_sg = ec2.SecurityGroup(self, "TargetInstanceSG", vpc=vpc, description="Security group for RDS instance")
-        target_instance_sg.add_ingress_rule(ec2.Peer.ipv4(vpc.vpc_cidr_block), ec2.Port.tcp(5432), "Allow access from the VPC")
-
-
-        # Create a security group for the DMS replication instance
-        dms_replication_security_group = ec2.SecurityGroup(
+        target_instance_sg = ec2.SecurityGroup(
             self,
-            "DMSReplicationSecurityGroup",
+            "TargetInstanceSG",
             vpc=vpc,
-            allow_all_outbound=True,
-            security_group_name="dms-replication-sg",
+            description="Security group for RDS instance",
+        )
+        target_instance_sg.add_ingress_rule(
+            ec2.Peer.ipv4(vpc.vpc_cidr_block),
+            ec2.Port.tcp(5432),
+            "Allow access from the VPC",
         )
 
         # Create a Secrets Manager secret to store the password for the Aurora cluster
@@ -62,7 +70,7 @@ class PostgresAuroraToInstanceStack(Stack):
             description="Secret for Aurora cluster password",
             generate_secret_string=secretsmanager.SecretStringGenerator(
                 exclude_punctuation=True,
-                secret_string_template=dumps({"username": 'dbuser'}),
+                secret_string_template=dumps({"username": "dbuser"}),
                 generate_string_key="password",
             ),
         )
@@ -74,56 +82,82 @@ class PostgresAuroraToInstanceStack(Stack):
             description="Secret for RDS instance password",
             generate_secret_string=secretsmanager.SecretStringGenerator(
                 exclude_punctuation=True,
-                secret_string_template=dumps({"username": 'dbuser'}),
+                secret_string_template=dumps({"username": "dbuser"}),
                 generate_string_key="password",
             ),
         )
-# Create an Aurora PostgreSQL cluster as the source database
-        source_cluster = rds.DatabaseCluster(self, "SourceCluster",
-            engine=rds.DatabaseClusterEngine.aurora_postgres(version=rds.AuroraPostgresEngineVersion.VER_13_4),
+
+        # Modify the replication parameter group associated with the Aurora PostgreSQL instance
+        parameter_group = rds.ParameterGroup(
+            self,
+            "ReplicationParameterGroup",
+            engine=rds.DatabaseClusterEngine.aurora_postgres(
+                version=rds.AuroraPostgresEngineVersion.VER_12_13
+            ),
+            parameters={"rds.logical_replication": "1"},
+        )
+
+        # Create an Aurora PostgreSQL cluster as the source database
+        source_cluster = rds.DatabaseCluster(
+            self,
+            "SourceCluster",
+            engine=rds.DatabaseClusterEngine.aurora_postgres(
+                version=rds.AuroraPostgresEngineVersion.VER_12_13
+            ),
             instance_props=rds.InstanceProps(
                 vpc=vpc,
-                instance_type=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.SMALL),
-                security_groups=[source_cluster_sg],
+                instance_type=ec2.InstanceType.of(
+                    ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MEDIUM
+                ),
+                security_groups=[
+                    source_cluster_sg,
+                    target_instance_sg,
+                ],
                 auto_minor_version_upgrade=True,
             ),
             credentials=rds.Credentials.from_secret(source_secret),
+            default_database_name=database_name,
+            port=5432,
             removal_policy=RemovalPolicy.DESTROY,
-        )
-
-       
-
-        # Create a security group for the target RDS instance
-        target_instance_security_group = ec2.SecurityGroup(
-            self,
-            "TargetInstanceSecurityGroup",
-            vpc=vpc,
-            allow_all_outbound=True,
-            security_group_name="target-instance-sg",
+            iam_authentication=True,
+            parameter_group=parameter_group,
         )
 
         # Create an RDS PostgreSQL instance with a read replica as the target database
-        target_instance = rds.DatabaseInstance(self, "TargetInstance",
-            engine=rds.DatabaseInstanceEngine.postgres(version=rds.PostgresEngineVersion.VER_13),
-            instance_type=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.SMALL),
+        target_instance = rds.DatabaseInstance(
+            self,
+            "InstancePostgreSQL",
+            engine=rds.DatabaseInstanceEngine.postgres(
+                version=rds.PostgresEngineVersion.VER_14_6
+            ),
+            # optional, defaults to m5.large
+            instance_type=ec2.InstanceType.of(
+                ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MEDIUM
+            ),
             vpc=vpc,
             security_groups=[target_instance_sg],
-            deletion_protection=False,
-            auto_minor_version_upgrade=True,
-            credentials=rds.Credentials.from_secret(target_secret),
-            removal_policy=RemovalPolicy.DESTROY,
-            )
-
-        # Create an Amazon DMS replication instance
-        dms_replication_instance = dms.CfnReplicationInstance(
-            self,
-            "DMSReplicationInstance",
-            replication_instance_class="dms.t2.micro",
-            replication_instance_identifier="dms-replication-instance",
-            allocated_storage=50,
-            engine_version="3.4.7",
             publicly_accessible=False,
-            vpc_security_group_ids=[dms_replication_security_group.security_group_id]
+            storage_encrypted=True,
+            storage_type=rds.StorageType.GP3,
+            iops=12000,
+            allocated_storage=500,
+            max_allocated_storage=1000,
+            port=5432,
+            multi_az=True,
+            credentials=rds.Credentials.from_secret(target_secret),
+            database_name=database_name,
+            iam_authentication=True,
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
+        target_reader_instance = rds.DatabaseInstanceReadReplica(
+            self,
+            "ReadReplicaAnalyze",
+            source_database_instance=target_instance,
+            instance_type=ec2.InstanceType.of(
+                ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MEDIUM
+            ),
+            vpc=vpc,
         )
 
         # Define the source and target endpoints for DMS
@@ -132,14 +166,14 @@ class PostgresAuroraToInstanceStack(Stack):
             "SourceEndpoint",
             endpoint_type="source",
             engine_name="aurora-postgresql",
-            database_name=source_cluster.cluster_identifier,
+            database_name=database_name,
+            server_name=source_cluster.cluster_endpoint.hostname,
             username=source_cluster.secret.secret_value_from_json(
                 "username"
             ).to_string(),
             password=source_cluster.secret.secret_value_from_json(
                 "password"
             ).to_string(),
-            server_name=source_cluster.cluster_endpoint.hostname,
             port=5432,
         )
 
@@ -148,16 +182,41 @@ class PostgresAuroraToInstanceStack(Stack):
             "TargetEndpoint",
             endpoint_type="target",
             engine_name="postgres",
-            database_name=target_instance.instance_identifier,
+            database_name=database_name,
+            server_name=target_instance.db_instance_endpoint_address,
             username=target_instance.secret.secret_value_from_json(
                 "username"
             ).to_string(),
             password=target_instance.secret.secret_value_from_json(
                 "password"
             ).to_string(),
-            server_name=target_instance.db_instance_endpoint_address,
             port=5432,
         )
+
+        vpc_private_subnet_ids = []
+        for sub in vpc.private_subnets:
+            vpc_private_subnet_ids.append(sub.subnet_id)
+
+        rep_sub = dms.CfnReplicationSubnetGroup(
+            self,
+            "DMSSubnetGroup",
+            replication_subnet_group_description="dms subnet group",
+            subnet_ids=vpc_private_subnet_ids,
+        )
+
+        # Create an Amazon DMS replication instance
+        dms_replication_instance = dms.CfnReplicationInstance(
+            self,
+            "DMSReplicationInstance",
+            replication_instance_class="dms.t3.large",
+            replication_instance_identifier="dms-replication-instance",
+            allocated_storage=50,
+            engine_version="3.4.7",
+            publicly_accessible=False,
+            vpc_security_group_ids=[target_instance_sg.security_group_id],
+            replication_subnet_group_identifier=rep_sub.ref,
+        )
+        dms_replication_instance.add_depends_on(rep_sub)
 
         # Create a DMS task to replicate data from the source to the target
         dms_task = dms.CfnReplicationTask(
@@ -170,18 +229,26 @@ class PostgresAuroraToInstanceStack(Stack):
                         "rule-id": "1",
                         "rule-name": "1",
                         "object-locator": {
-                            "schema-name": "%",
+                            "schema-name": "public",
                             "table-name": "%"
                         },
                         "rule-action": "include"
                     }
                 ]
             }""",
+            replication_task_settings=dumps(
+                {
+                    "FailOnNoTablesCaptured": "false",
+                }
+            ),
             source_endpoint_arn=source_endpoint.ref,
             target_endpoint_arn=target_endpoint.ref,
             replication_instance_arn=dms_replication_instance.ref,
             migration_type="full-load-and-cdc",
         )
+        dms_task.add_depends_on(source_endpoint)
+        dms_task.add_depends_on(target_endpoint)
+        dms_task.add_depends_on(dms_replication_instance)
 
         CfnOutput(self, "OutputSourceEndpoint", value=source_endpoint.ref)
         CfnOutput(self, "OutputTargetEndpoint", value=target_endpoint.ref)
