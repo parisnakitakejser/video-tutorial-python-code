935e4567cde22ba4775e4fc36ad1224271ed3e71
Paris Nakita Kejser <hi@pnk.sh>
2024-02-15 11:07:24 +0100

    update names in settings

--- Kafka/sample/settings.py
+++ Kafka/sample/settings.py
@@ -5,5 +5,5 @@ BOOTSTRAP_SERVERS = ['localhost:29092']
 AVRO_SCHEMA_FILE = 'booking.avsc'
 
 # AWS GLUE settings
-GLUE_STREAM_SCHEMA_REGISTRY = 'test'
-GLUE_SCHEMA = 'demo'
+GLUE_STREAM_SCHEMA_REGISTRY = 'my-first-glue-registry'
+GLUE_SCHEMA = 'my-schema-name'
